90df14b7a48bce12f8680a3399a830ac740fefb9
Michael Short <sho16010@byui.edu>
2018-06-02 21:58:13 -0700

    Getting Close

--- cit230/assignments/lesson-6/scripts/currentdate.js
+++ cit230/assignments/lesson-6/scripts/currentdate.js
@@ -1,18 +1,17 @@
 function getTodaysDate(date) {
-  var monthNames = [
+    var monthNames = [
       "January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
   ];
-  var days = [
+    var days = [
       "Sunday", "Monday", "Tuesday", "Wednesday",
       "Thursday", "Friday", "Saturday"
   ];
 
-  var weekday = date.getDay();
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
+    var weekday = date.getDay();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
 
     return days[weekday] + ',' + String.fromCharCode(160) + day + String.fromCharCode(160) + monthNames[monthIndex] + String.fromCharCode(160) + year;
 }
-

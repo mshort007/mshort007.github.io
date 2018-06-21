5e9a0e19162a14131cbd0e11eabe6f018c2ac002
Michael Short <sho16010@byui.edu>
2018-06-20 19:41:45 -0700

    Added API-Test
    API-Test and Portal Page Updated

---
+++ cit230/assignments/lesson-9/scripts/weatherapi.js
@@ -0,0 +1,11 @@
+var weatherRequest = new XMLHttpRequest();
+var apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=dd14403260f2bc5da5d473915847cf4f&units=imperial';
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+} // end of weatherRequest.onload function

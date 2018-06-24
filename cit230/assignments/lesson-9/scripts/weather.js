37511d290070eafa21df8a0a37b99932fcb0b9d7
Michael Short <sho16010@byui.edu>
2018-06-23 22:34:30 -0700

    weather condition icon
    update icon for current condition

--- cit230/assignments/lesson-9/scripts/weather.js
+++ cit230/assignments/lesson-9/scripts/weather.js
@@ -13,6 +13,10 @@ frWeatherRequest.onload =  function () {
     document.getElementById('dailylow').innerHTML = weatherData.main.temp_min;
     document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
 
+    var iconcode = weatherData.weather[0].icon;
+    var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+
 } // end of frWeatherRequest.onload function
 
 // Franklin: 4156210

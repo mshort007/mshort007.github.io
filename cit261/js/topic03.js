13acde751911cbc59ea12360308c8e4e0b5554f8
Michael Short <sho16010@byui.edu>
2020-03-03 01:05:10 -0800

    Topic 3 complete and ready

--- cit261/js/topic03.js
+++ cit261/js/topic03.js
@@ -1,27 +1,46 @@
-
 var JSONString = '{ "year": 2012, "make": "Volkswagen", "model": "Jetta Sportswagen TDI", "color": "Tornado Red", "milage": 51234 }';
-var parsedObject = JSON.parse(JSONString);
 var bigFamily = { father:"Dad", mother:"Mom", child1:"Michael", child2:"Ian", child3:"Mia", child4:"Linda", child5:"Jacob", child6:"Keith" }; 
-var parsedObjectContent = "";
-var rawObjectContent = "";
 
 // JSON Parse 
-document.getElementById("rawString").innerHTML = JSONString;
+function parseString() {
+	// Parse JSON String
+	let parsedObjectContent = "";
+	let parsedObject = JSON.parse(JSONString);	
 
-//prepare and output contents of parsedObject
-for (x in parsedObject) {
-	parsedObjectContent += "parsedObject[" + x + "] = [" + parsedObject[x] + "]<br>";
+	//Format and output contents of parsedObject
+	for (x in parsedObject) {
+		parsedObjectContent += "Object[" + x + "] = " + parsedObject[x] + "<br>";
+	}
+	
+	document.getElementById("parsedString").innerHTML = parsedObjectContent;
 }
-document.getElementById("parsedString").innerHTML = parsedObjectContent;
 
+function clearParse() {
+	document.getElementById("parsedString").innerHTML = "Press Parse to Parse String into Object";
+}
 
 // JSON Stringify
-
-//prepare and output contents of bigFamily
-for (x in bigFamily) {
-	rawObjectContent += "bigFamily[" + x + "] = [" + bigFamily[x] + "]<br>";
+function stringifyObject() {
+	//output the stringified version of object bigFamily
+	document.getElementById("stringifiedObject").innerHTML = JSON.stringify(bigFamily);
 }
-document.getElementById("rawObject").innerHTML = rawObjectContent;
 
-//output the stringified version of object bigFamily
-document.getElementById("stringifiedObject").innerHTML = JSON.stringify(bigFamily);
+function clearStringify() {
+	document.getElementById("stringifiedObject").innerHTML = "Press Stringify to turn Object into JSON Text";
+}
+
+// Page Prep
+onload = function(){
+	clearParse();
+	clearStringify();
+	
+	// Populate Raw JSON String on page 
+	document.getElementById("rawString").innerHTML = JSONString;
+	
+	// Populate Object Contents
+	let rawObjectContent = "";
+	for (x in bigFamily) {
+		rawObjectContent += "Object[" + x + "] = " + bigFamily[x] + "<br>";
+	}
+	document.getElementById("rawObject").innerHTML = rawObjectContent;
+}

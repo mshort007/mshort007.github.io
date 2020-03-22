05f96ad1f26a46fa890c25fa0c3a6f9ecbf13832
Michael Short <sho16010@byui.edu>
2020-03-21 21:59:02 -0700

    Start of Final Project with Basic Layout and API Test

---
+++ cit261finalproject/js/nutritionAPI.js
@@ -0,0 +1,21 @@
+var itemSearchAPI = "https://trackapi.nutritionix.com/v2/search/item";
+var appId = "0179d55a";
+var appKey = "d674e8619657d62a79ee68f55cfc451d"
+
+function doItemSearch() {
+	let myRequest = new XMLHttpRequest();
+	// This will come from page, but first we need to have it working with a well known product
+	let searchQuery = "0047500010832";
+	myRequest.setRequestHeader('x-app-id', appId);
+	myRequest.setRequestHeader('x-app-key', appKey);
+	let requestUrl = itemSearchAPI + '?upc=' + searchQuery + '&claims=false';
+	
+	myRequest.open('GET', requestUrl, true);
+	myRequest.send();
+	
+	myRequest.onload = function() {
+		let myResponse = JSON.parse(myRequest.responseText);
+		// Lets see if this worked so we can pick it apart.
+		console.log(myResponse);
+	}
+}
